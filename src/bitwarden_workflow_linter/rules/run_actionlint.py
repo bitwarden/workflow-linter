04bad820979440ac15c03079dab1e1dea3516dff
Amy Galles <9685081+AmyLGalles@users.noreply.github.com>
2024-12-27 11:51:40 -0500

    making output more specific

--- src/bitwarden_workflow_linter/rules/run_actionlint.py
+++ src/bitwarden_workflow_linter/rules/run_actionlint.py
@@ -10,13 +10,14 @@ from ..utils import LintLevels, Settings
 
 class RunActionlint(Rule):
     def __init__(self, settings: Settings = None) -> None:
-        self.message = "Actionlint must pass"
+        self.message = "Actionlint must pass without errors"
         self.on_fail: LintLevels = LintLevels.ERROR
         self.compatibility: List[Union[Workflow, Job, Step]] = [Job]
         self.settings: Settings = settings
 
     def fn(self, obj: Job) -> Tuple[bool, str]:
         result = subprocess.run(["actionlint"], capture_output=True, text=True)
-        print(result.stdout)
-        print(result.stderr)
-        return True, self.message
+        if result.returncode == 1:
+            print(result.stdout)
+            return False, self.message
+        return True, ""
