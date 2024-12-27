d016a6d82c0f0ed34b706fba003d2e7f0222d4b3
Amy Galles <9685081+AmyLGalles@users.noreply.github.com>
2024-12-27 11:53:02 -0500

    making output more specific

--- src/bitwarden_workflow_linter/rules/run_actionlint.py
+++ src/bitwarden_workflow_linter/rules/run_actionlint.py
@@ -20,4 +20,6 @@ class RunActionlint(Rule):
         if result.returncode == 1:
             print(result.stdout)
             return False, self.message
+        elif result.returncode > 1:
+            return False, result.stderr
         return True, ""
