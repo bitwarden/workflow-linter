0187ed53219119f78c949a1caa73bff990768b07
Amy Galles <9685081+AmyLGalles@users.noreply.github.com>
2024-12-31 09:13:32 -0800

    fixng string interpolation

--- src/bitwarden_workflow_linter/rules/run_actionlint.py
+++ src/bitwarden_workflow_linter/rules/run_actionlint.py
@@ -17,6 +17,7 @@ def check_actionlint():
     and installs actionlint
     """
     Platform = platform.system()
+    error = f"An unknown error occurred on platform {Platform}"
     try:
         subprocess.run(["actionlint", '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
         return True, ""
@@ -44,7 +45,7 @@ def check_actionlint():
             except subprocess.CalledProcessError:
                 error = "Failed to install Actionlint. Please check your Chocolatey installation or manually install it."
                 return False, error
-    return False, "An unknown error occurred on platform {Platform}"
+    return False, error
             
 class RunActionlint(Rule):
     """Rule to run actionlint as part of workflow linter V2.
