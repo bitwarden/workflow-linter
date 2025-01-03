b5b99fb4b96dc37d6dc3dbbbfd61f89e0c6c7884
Amy Galles <9685081+AmyLGalles@users.noreply.github.com>
2025-01-03 15:45:39 -0800

    loading test content from a file

--- tests/rules/test_run_actionlint.py
+++ tests/rules/test_run_actionlint.py
@@ -10,96 +10,29 @@ from src.bitwarden_workflow_linter.rules.run_actionlint import RunActionlint
 yaml = YAML()
 
 
-@pytest.fixture(name="correct_workflow")
-def fixture_correct_workflow():
-    workflow = """\
----
-name: test
-on:
-  workflow_dispatch:
-  pull_request:
-
-jobs:
-  job-key:
-    name: Test
-    runs-on: ubuntu-latest
-    steps:
-      - name: Test
-        run: echo test
-
-  call-workflow:
-    uses: bitwarden/server/.github/workflows/workflow-linter.yml@master
-
-  test-normal-action:
-    name: Download Latest
-    runs-on: ubuntu-20.04
-    steps:
-      - name: Checkout
-        uses: actions/checkout@2541b1294d2704b0964813337f33b291d3f8596b
-
-      - run: |
-          echo test
-
-  test-local-action:
-    name: Testing a local action call
-    runs-on: ubuntu-20.04
-    steps:
-      - name: local-action
-        uses: ./version-bump
-"""
-    return WorkflowBuilder.build(workflow=yaml.load(workflow), from_file=False)
+# @pytest.fixture(name="correct_workflow")
+# def fixture_correct_workflow():
+#     workflow = "tests/fixtures/test_workflow.yaml"
+#     return WorkflowBuilder.build(workflow)
 
 
-@pytest.fixture(name="incorrect_workflow")
-def fixture_incorrect_workflow():
-    workflow = """\
----
-names: test
-on:
-  workflow_dispatch:
-  pull_request:
-
-jobs:
-  job-key:
-    name: Test
-    runs-on: ubuntu-latest
-    steps:
-      - name: Test
-        run: echo test
-
-  call-workflow:
-    uses: bitwarden/server/.github/workflows/workflow-linter.yml@master
-
-  test-normal-action:
-    name: Download Latest
-    runs-on: ubuntu-20.04
-    steps:
-      - name: Checkout
-        uses: actions/checkout@2541b1294d2704b0964813337f33b291d3f8596b
-
-      - run: |
-          echo test
-
-  test-local-action:
-    name: Testing a local action call
-    runs-on: ubuntu-20.04
-    steps:
-      - name: local-action
-        uses: ./version-bump
-"""
-    return WorkflowBuilder.build(workflow=yaml.load(workflow), from_file=False)
-
+# @pytest.fixture(name="incorrect_workflow")
+# def fixture_incorrect_workflow():
+#     workflow = "tests/fixtures/test_workflow_incorrect.yaml"
+#     return WorkflowBuilder.build(workflow)
 
 @pytest.fixture(name="rule")
 def fixture_rule():
     return RunActionlint()
 
 
-def test_rule_on_correct_workflow(rule, correct_workflow):
+def test_rule_on_correct_workflow(rule):
+    correct_workflow =  WorkflowBuilder.build("tests/fixtures/test_workflow.yaml")
     result, _ = rule.fn(correct_workflow)
     assert result is True
 
 
-def test_rule_on_incorrect_workflow(rule, incorrect_workflow):
+def test_rule_on_incorrect_workflow(rule):
+    incorrect_workflow = WorkflowBuilder.build("tests/fixtures/test_workflow_incorrect.yaml")
     result, _ = rule.fn(incorrect_workflow)
     assert result is False
