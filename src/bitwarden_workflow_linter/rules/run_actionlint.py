3ae7b8e54113450d03863ca39657ad69dddb4a12
Amy Galles <9685081+AmyLGalles@users.noreply.github.com>
2025-01-06 15:05:45 -0800

    fixing location of error

--- src/bitwarden_workflow_linter/rules/run_actionlint.py
+++ src/bitwarden_workflow_linter/rules/run_actionlint.py
@@ -48,7 +48,9 @@ please check your package installer or manually install it",
                     print('******')
                     return True, ""
             except (FileNotFoundError, subprocess.CalledProcessError):
+                print('******')
                 print(result.stdout)
+                print('******')
                 error = "Failed to install Actionlint1. \
 Please check your package manager or manually install it."
                 return False, error
