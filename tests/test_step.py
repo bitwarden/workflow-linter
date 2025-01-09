19986a14af5cb70393efe37f371b4247d8660294
Andy Pixley <3723676+pixman20@users.noreply.github.com>
2025-01-09 13:07:58 -0500

    [BRE-541] Adding tests for Step

--- tests/test_step.py
+++ tests/test_step.py
@@ -36,6 +36,45 @@ with:
     return Step.init(0, "default", step_yaml)
 
 
+@pytest.fixture(name="uses_step_no_comments")
+def fixture_uses_step_no_comments():
+    step_str = """\
+name: Download Artifacts
+uses: bitwarden/download-artifacts@main
+with:
+    workflow: upload-test-artifacts.yml
+    artifacts: artifact
+    path: artifact
+    branch: main
+
+"""
+    yaml = YAML()
+    step_yaml = yaml.load(step_str)
+    return Step.init(0, "default", step_yaml)
+
+
+@pytest.fixture(name="uses_step_no_ref")
+def fixture_uses_step_no_ref():
+    step_str = """\
+name: Run Local Workflow
+uses: ./.github/workflows/_local.yml
+"""
+    yaml = YAML()
+    step_yaml = yaml.load(step_str)
+    return Step.init(0, "default", step_yaml)
+
+
+@pytest.fixture(name="uses_step_no_ref_with_comments")
+def fixture_uses_step_no_ref_with_comments():
+    step_str = """\
+name: Run Local Workflow
+uses: ./.github/workflows/_local.yml # A comment
+"""
+    yaml = YAML()
+    step_yaml = yaml.load(step_str)
+    return Step.init(0, "default", step_yaml)
+
+
 def test_step_default(default_step):
     assert default_step.key == 0
     assert default_step.job == "default"
@@ -74,5 +113,32 @@ def test_step_keyword_field(uses_step):
 def test_step_comment(uses_step):
     assert uses_step.key == 0
     assert uses_step.job == "default"
-    assert uses_step.uses_comment is not None
+    assert uses_step.uses_ref == "main"
+    assert uses_step.uses_version == "v1.0.0"
     assert uses_step.uses_comment == "# v1.0.0"
+
+
+def test_step_no_comments(uses_step_no_comments):
+    assert uses_step_no_comments.key == 0
+    assert uses_step_no_comments.job == "default"
+    assert uses_step_no_comments.uses_ref == "main"
+    assert uses_step_no_comments.uses_version is None
+    assert uses_step_no_comments.uses_comment is None
+
+
+def test_step_no_ref(uses_step_no_ref):
+    assert uses_step_no_ref.key == 0
+    assert uses_step_no_ref.job == "default"
+    assert uses_step_no_ref.uses_ref is None
+    assert uses_step_no_ref.uses_version is None
+    assert uses_step_no_ref.uses_comment is None
+
+
+def test_step_no_ref_with_comments(uses_step_no_ref_with_comments):
+    assert uses_step_no_ref_with_comments.key == 0
+    assert uses_step_no_ref_with_comments.job == "default"
+    assert uses_step_no_ref_with_comments.uses_ref is None
+    assert (
+        uses_step_no_ref_with_comments.uses_version == "comment"
+    )  # We are not currently validating the version matches a specific format
+    assert uses_step_no_ref_with_comments.uses_comment == "# A comment"
