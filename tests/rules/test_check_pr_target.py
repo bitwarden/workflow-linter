a0c84d587e06f8716e95780ae527ad43f33db888
Amy Galles <9685081+AmyLGalles@users.noreply.github.com>
2025-02-03 10:49:31 -0800

    pushing bug for collaboration

--- tests/rules/test_check_pr_target.py
+++ tests/rules/test_check_pr_target.py
@@ -23,7 +23,7 @@ on:
 jobs:
   check-run:
     name: Check PR run
-    uses: bitwarden/gh-actions/.github/workflows/check-run.yml@main
+    uses: bitwarden
   
   quality:
     name: Quality scan
@@ -31,7 +31,7 @@ jobs:
     steps:
       - run: echo test
  
-   dependent-job:
+  dependent-job:
      name: Another Dependent Job
      needs:
        - quality
@@ -70,7 +70,7 @@ on:
 jobs:
   check-run:
     name: Check PR run
-    uses: bitwarden/gh-actions/.github/workflows/check-run.yml@main
+    uses: something/gh-actions/.github/workflows/check-run.yml@main
 
   job-key:
     runs-on: ubuntu-22.04
@@ -100,35 +100,60 @@ jobs:
 """
     return WorkflowBuilder.build(workflow=yaml.load(workflow), from_file=False)
 
+# @pytest.fixture(name="dependent_missing_check_workflow")
+# def dependent_missing_check_workflow():
+#     workflow = """\
+# ---
+# on:
+#   workflow_dispatch:
+#   pull_request_target:
+
+# jobs:
+#   check-run:
+#     name: Check PR run
+#     uses: bitwarden/gh-actions/.github/workflows/check-run.yml@main
+  
+#   quality:
+#     name: Quality scan
+#     needs: check-run
+#     steps:
+#       - run: echo test
+ 
+#   dependent-job:
+#      name: Another Dependent Job
+#      needs:
+#        - quality
+#      steps:
+#        - run: echo another dependent job
+# """
+#     return WorkflowBuilder.build(workflow=yaml.load(workflow), from_file=False)
+
 @pytest.fixture(name="rule")
 def fixture_rule():
     return RuleCheckPrTarget()
 
 def test_rule_on_correct_workflow(rule, correct_workflow):
-    obj = correct_workflow
-
     result, message = rule.fn(correct_workflow)
     assert result is True
     assert message == ""
 
 
 def test_rule_on_no_checkworkflow(rule, no_check_workflow):
-    obj = no_check_workflow
-
     result, message = rule.fn(no_check_workflow)
     assert result is False
     assert message == message
 
 def test_rule_on_no_target_workflow(rule, no_target_workflow):
-    obj = no_target_workflow
-
     result, message = rule.fn(no_target_workflow)
     assert result is True
     assert message == ""
 
 def test_rule_on_jobs_without_needs(rule, no_needs_workflow):
-    obj = no_needs_workflow
-
     result, message = rule.fn(no_needs_workflow)
     assert result is False
     assert message == message, "check-run is missing from the following jobs in the workflow: quality"
+
+# def test_rule_on_dependencies_without_check(rule, dependent_missing_check_workflow):
+#     result, message = rule.fn(dependent_missing_check_workflow)
+#     assert result is False
+#     assert message == message, "check-run is missing from the following jobs in the workflow: dependent-job"
