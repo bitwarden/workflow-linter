63fdc808d2f46db40771001c3cf179e74a87bb7d
Amy Galles <9685081+AmyLGalles@users.noreply.github.com>
2025-02-06 12:18:16 -0800

    adding tests for fixed rule

--- tests/rules/test_check_pr_target.py
+++ tests/rules/test_check_pr_target.py
@@ -10,7 +10,7 @@ from src.bitwarden_workflow_linter.rules.check_pr_target import (
 )
 
 yaml = YAML()
-message = "Check-run job must be included when pull_request_target is used"
+message = "A check-run job must be included as a direct job dependency when pull_request_target is used and the workflow may only apply to runs on the main branch"
 
 @pytest.fixture(name="correct_workflow")
 def fixture_correct_workflow():
@@ -20,6 +20,8 @@ on:
   workflow_dispatch:
   pull_request_target:
     types: [opened, synchronize]
+    branches:
+      - 'main'
 
 jobs:
   check-run:
@@ -50,6 +52,8 @@ on:
   workflow_dispatch:
   pull_request_target:
     types: [opened, synchronize]
+    branches:
+      - 'main'
 
 jobs:
   job-key:
@@ -67,6 +71,8 @@ on:
   workflow_dispatch:
   pull_request_target:
     types: [opened, synchronize]
+    branches:
+      - 'main'
 
 jobs:
   check-run:
@@ -109,6 +115,9 @@ on:
   workflow_dispatch:
   pull_request_target:
     types: [opened, synchronize]
+    branches:
+      - 'main'
+
 jobs:
   check-run:
     name: Check PR run
@@ -124,6 +133,99 @@ jobs:
      name: Another Dependent Job
      needs:
        - quality
+     steps:
+       - run: echo another dependent job
+"""
+    return WorkflowBuilder.build(workflow=yaml.load(workflow), from_file=False)
+
+@pytest.fixture(name="no_branches_workflow")
+def fixture_no_branches_workflow():
+    workflow = """\
+---
+on:
+  workflow_dispatch:
+  pull_request_target:
+    types: [opened, synchronize]
+
+jobs:
+  check-run:
+    name: Check PR run
+    uses: bitwarden/gh-actions/.github/workflows/check-run.yml@main
+  
+  quality:
+    name: Quality scan
+    needs: check-run
+    steps:
+      - run: echo test
+ 
+  dependent-job:
+     name: Another Dependent Job
+     needs:
+       - quality
+       - check-run
+     steps:
+       - run: echo another dependent job
+"""
+    return WorkflowBuilder.build(workflow=yaml.load(workflow), from_file=False)
+
+@pytest.fixture(name="bad_branches_workflow")
+def fixture_bad_branches_workflow():
+    workflow = """\
+---
+on:
+  workflow_dispatch:
+  pull_request_target:
+    types: [opened, synchronize]
+    branches:
+      - 'main'
+      - 'not main'
+
+jobs:
+  check-run:
+    name: Check PR run
+    uses: bitwarden/gh-actions/.github/workflows/check-run.yml@main
+  
+  quality:
+    name: Quality scan
+    needs: check-run
+    steps:
+      - run: echo test
+ 
+  dependent-job:
+     name: Another Dependent Job
+     needs:
+       - quality
+       - check-run
+     steps:
+       - run: echo another dependent job
+"""
+    return WorkflowBuilder.build(workflow=yaml.load(workflow), from_file=False)
+
+@pytest.fixture(name="two_failures_workflow")
+def fixture_two_failures_workflow():
+    workflow = """\
+---
+on:
+  workflow_dispatch:
+  pull_request_target:
+    types: [opened, synchronize]
+
+jobs:
+  check-run:
+    name: Check PR run
+    uses: bitwarden/some-other-repo/.github/workflows/check-run.yml@main
+  
+  quality:
+    name: Quality scan
+    needs: check-run
+    steps:
+      - run: echo test
+ 
+  dependent-job:
+     name: Another Dependent Job
+     needs:
+       - quality
+       - check-run
      steps:
        - run: echo another dependent job
 """
@@ -158,4 +260,18 @@ def test_rule_on_dependencies_without_check(rule, dependent_missing_check_workfl
     result, message = rule.fn(dependent_missing_check_workflow)
     assert result is False
     assert message == message, "check-run is missing from the following jobs in the workflow: dependent-job"
-    
+
+def test_rule_on_no_target_workflow(rule, no_branches_workflow):
+    result, message = rule.fn(no_branches_workflow)
+    assert result is False
+    assert message == "Workflows using pull_request_target can only target the main branch"
+
+def test_rule_on_no_target_workflow(rule, bad_branches_workflow):
+    result, message = rule.fn(bad_branches_workflow)
+    assert result is False
+    assert message == "Workflows using pull_request_target can only target the main branch"
+
+def test_rule_on_no_target_workflow(rule, two_failures_workflow):
+    result, message = rule.fn(two_failures_workflow)
+    assert result is False
+    assert message == "Workflows using pull_request_target can only target the main branch\nA check-run job must be included as a direct job dependency when pull_request_target is used"
