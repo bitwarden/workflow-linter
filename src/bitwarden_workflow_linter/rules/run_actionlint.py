37efc2efb64d360cf30405d27a29be1099345947
Amy Galles <9685081+AmyLGalles@users.noreply.github.com>
2025-02-13 14:55:44 -0800

    trying with globbing

--- src/bitwarden_workflow_linter/rules/run_actionlint.py
+++ src/bitwarden_workflow_linter/rules/run_actionlint.py
@@ -27,7 +27,8 @@ def install_actionlint(platform_system: str, version: str) -> Tuple[bool, str]:
     elif platform_system.startswith("Win"):
         try:
             print(f"Version is {version}")
-            subprocess.run(["choco", "install", "actionlint", "-y", f"--version='1.6.17'"], check=True)
+            subprocess.run(["choco install actionlint -y --version='1.6.17'"], check=True, shell=True)
+            # subprocess.run(["choco", "install", "actionlint", "-y", f"--version='1.6.17'"], check=True)
             return True, ""
         except (FileNotFoundError, subprocess.CalledProcessError):
             return False, f"{error} : check Choco installation"
