7c375dec49799c3c78b38ff202d1264d9762736d
Amy Galles <9685081+AmyLGalles@users.noreply.github.com>
2025-02-14 09:59:46 -0800

    managing version through settings file

--- src/bitwarden_workflow_linter/rules/run_actionlint.py
+++ src/bitwarden_workflow_linter/rules/run_actionlint.py
@@ -11,33 +11,34 @@ from ..models.workflow import Workflow
 from ..utils import LintLevels, Settings
 
 
-def install_actionlint(platform_system: str) -> Tuple[bool, str]:
+def install_actionlint(platform_system: str, version: str) -> Tuple[bool, str]:
     """If actionlint is not installed, detects OS platform
     and installs actionlint"""
+    print("reached install")
 
     error = f"An error occurred when installing Actionlint on {platform_system}"
-
     if platform_system.startswith("Linux"):
-        return install_actionlint_source(error)
+        return install_actionlint_source(error, version)
     elif platform_system == "Darwin":
         try:
-            subprocess.run(["brew", "install", "actionlint"], check=True)
+            subprocess.run(["brew", "install", "actionlint"], check=True) #homebrew does not have previous versions of actionlint available
             return True, ""
         except (FileNotFoundError, subprocess.CalledProcessError):
             return False, f"{error} : check Brew installation"
     elif platform_system.startswith("Win"):
         try:
-            subprocess.run(["choco", "install", "actionlint", "-y", "--version='1.6.17'"], check=True)
+            print(f"Version is {version}")
+            subprocess.run(["choco", "install", "actionlint", "-y", f"--version='{version}'"], check=True)
             return True, ""
         except (FileNotFoundError, subprocess.CalledProcessError):
             return False, f"{error} : check Choco installation"
     return False, error
 
 
-def install_actionlint_source(error) -> Tuple[bool, str]:
+def install_actionlint_source(error, version) -> Tuple[bool, str]:
     """Install Actionlint Binary from provided script"""
     url = "https://raw.githubusercontent.com/rhysd/actionlint/main/scripts/download-actionlint.bash"
-    version = "1.6.17"
+    version = version
     request = urllib.request.urlopen(url)
     with open("download-actionlint.bash", "wb+") as fp:
         fp.write(request.read())
@@ -48,8 +49,9 @@ def install_actionlint_source(error) -> Tuple[bool, str]:
         return False, error
 
 
-def check_actionlint(platform_system: str) -> Tuple[bool, str]:
+def check_actionlint(platform_system: str, version: str) -> Tuple[bool, str]:
     """Check if the actionlint is in the system's PATH."""
+    print("in check_actionlint")
     try:
         subprocess.run(
             ["actionlint", "--version"],
@@ -65,8 +67,19 @@ def check_actionlint(platform_system: str) -> Tuple[bool, str]:
 please check your package installer or manually install it",
         )
     except FileNotFoundError:
-        return install_actionlint(platform_system)
-
+        print("file not found")
+        is_local = subprocess.run(
+                    ["ls | grep '^actionlint'"],
+                    capture_output=True,
+                    text=True,
+                    check=False,
+                    shell=True
+                )
+        if is_local.stdout:
+            return True, "."
+        else:
+            print("reached else")
+            return install_actionlint(platform_system, version)
 
 class RunActionlint(Rule):
     """Rule to run actionlint as part of workflow linter V2."""
@@ -83,7 +96,7 @@ class RunActionlint(Rule):
                 "Running actionlint without a filename is not currently supported"
             )
 
-        installed, location = check_actionlint(platform.system())
+        installed, location = check_actionlint(platform.system(), self.settings.actionlint_version)
         if installed:
             if location:
                 result = subprocess.run(
