06b6d21450bb5bd348628549863fdc0503bcf2a1
Amy Galles <9685081+AmyLGalles@users.noreply.github.com>
2025-02-21 14:52:34 -0800

    removing redundant local check

--- src/bitwarden_workflow_linter/rules/run_actionlint.py
+++ src/bitwarden_workflow_linter/rules/run_actionlint.py
@@ -59,20 +59,10 @@ def check_actionlint(platform_system: str, version: str) -> Tuple[bool, str]:
 please check your package installer or manually install it",
         )
     except FileNotFoundError:
-        is_local = subprocess.run(
-                    ["ls | grep '^actionlint'"],
-                    capture_output=True,
-                    text=True,
-                    check=False,
-                    shell=True
-                )
-        if is_local.stdout:
-            return True, "."
+        if os.path.exists("./actionlint"):
+            return True, "./actionlint"
         else:
-            if os.path.exists("./actionlint"):
-                return True, "./actionlint"
-            else:
-                return install_actionlint(platform_system)
+            return install_actionlint(platform_system)
 
 
 class RunActionlint(Rule):
